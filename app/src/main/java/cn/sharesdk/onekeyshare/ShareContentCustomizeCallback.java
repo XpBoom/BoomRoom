dd43bd6b364f5c1dde3b94473056c614b7bf859f
XpBoom <250046582@qq.com>
2016-12-13 17:03:13 +0800

    初始提交

---
+++ app/src/main/java/cn/sharesdk/onekeyshare/ShareContentCustomizeCallback.java
@@ -0,0 +1,21 @@
+/*
+ * ������վ:http://www.mob.com
+ * ����֧��QQ: 4006852216
+ * �ٷ�΢��:ShareSDK   ����������°汾�Ļ������ǽ����һʱ��ͨ��΢�Ž��汾�����������͸��������ʹ�ù��������κ����⣬Ҳ����ͨ��΢��������ȡ����ϵ�����ǽ�����24Сʱ�ڸ���ظ���
+ *
+ * Copyright (c) 2013�� mob.com. All rights reserved.
+ */
+
+package cn.sharesdk.onekeyshare;
+
+import cn.sharesdk.framework.Platform;
+import cn.sharesdk.framework.Platform.ShareParams;
+
+
+
+/** �Զ��岻ͬƽ̨����ͬ���ݵĽӿ� */
+public interface ShareContentCustomizeCallback {
+
+	public void onShare(Platform platform, ShareParams paramsToShare);
+
+}
